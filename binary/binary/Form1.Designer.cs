3bb9e69d122d32f92eaf616e3267ab8458e56a73
Dinçer Ünal <dincer.unal0652@gmail.com>
2015-03-26 18:34:33 +0200

    BinaryTree
    Create a random number and insert to binary tree

---
+++ binary/binary/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace binary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printbox = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.adresbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // numbox
+            // 
+            this.numbox.Location = new System.Drawing.Point(12, 12);
+            this.numbox.Name = "numbox";
+            this.numbox.Size = new System.Drawing.Size(90, 183);
+            this.numbox.TabIndex = 0;
+            this.numbox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sayilari Yaz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printbox
+            // 
+            this.printbox.Location = new System.Drawing.Point(194, 13);
+            this.printbox.Name = "printbox";
+            this.printbox.Size = new System.Drawing.Size(66, 182);
+            this.printbox.TabIndex = 2;
+            this.printbox.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // adresbox
+            // 
+            this.adresbox.Location = new System.Drawing.Point(142, 13);
+            this.adresbox.Name = "adresbox";
+            this.adresbox.Size = new System.Drawing.Size(46, 182);
+            this.adresbox.TabIndex = 4;
+            this.adresbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.adresbox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.printbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox numbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox printbox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox adresbox;
+    }
+}
+
---
+++ binary/binary/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace binary
+{
+ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] adress = new int[307];
+        int[] sayilar=new int[300];
+        ArrayList lstNumbers = new ArrayList();
+        ArrayList binary;
+
+        public void numbergenerate()
+        {
+            Random rndNumber = new Random();
+            int number = rndNumber.Next(1, 100000);
+            lstNumbers.Add(number);
+           
+            int count = 0;
+            do
+            {
+                number = rndNumber.Next(1, 100000);
+                if (!lstNumbers.Contains(number))
+                {
+                    lstNumbers.Add(number);
+                    count++;
+                }
+            } while (count < 299); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Visible = true; 
+            oku();
+            for (int i = 0; i < 300; i++)
+                numbox.Text += sayilar[i] + "\n";
+            button1.Visible = false;
+        }
+        public int homeadress(int key)
+        {
+            return key % 307;
+        }
+        public int increment(int key)
+        {
+            return (key / 307) % 307;
+        }
+        
+        public void DosyayaYaz()
+        {
+            numbergenerate();
+            StreamWriter dosya = new StreamWriter("kayit.txt");
+            for (int i = 0; i < 300; i++)
+                dosya.WriteLine(lstNumbers[i]);
+            dosya.Close();
+        }
+
+        public void insert(int key)
+        {
+            binary = new ArrayList();
+            if (adress[homeadress(key)] == -1)
+                adress[homeadress(key)] = key;
+            else
+            {
+                binary.Add(key);
+                binary.Add(homeadress(key));
+                tree(key);
+                yerlestir(key);
+            }
+        }
+        public void tree(int key)// tree oluşturacak
+        {
+            int i = 1;
+            while (true)
+            {
+                i++;
+               if(i==2 || i==4 || i==8 || i==16 ||i==32 ||i==64||i==128||i==256 ||i==512 ||i==1024 ||i==2048 ||i==4096 || i==8192 || i==16384 || i== 32768)
+                    binary.Add(   ((int)binary[parent(i)]  +increment(key))   %307      );
+               else
+                   binary.Add(((int)binary[parent(i)] + increment(adress[(int)binary[parent(parent(i))]]))%307   );
+
+                if(adress[((int)binary[i])]==-1)
+                    break;
+                i++;
+                binary.Add(((int)binary[parent(i)] + increment(adress[(int)binary[parent(i)]]) ) %307
+                    );
+                if (adress[((int)binary[i])] == -1)
+                    break;
+            }
+
+        }
+        public int parent(int i)
+        {
+            return i / 2;
+        }
+        public int left(int i)
+        {
+            return i*2;
+        }
+        public int right(int i)
+        {
+            return (i * 2) + 1;
+        }
+
+        public void yerlestir(int key)
+        {
+            int temp=binary.Count-1;//ağaçtaki boşyer
+            
+            while (true)
+            {
+                if (parent(temp) == 0)
+                    break;
+                else if (temp % 2 == 0) //boş yer sol çocukta
+                {
+                    temp = parent(temp);
+                    if (parent(temp) != 0)
+                    {
+                        if (temp % 2 == 1)//sağın solu
+                        {
+                            adress[(int)binary[left(temp)]] = adress[(int)binary[parent(temp)]];
+                            adress[(int)binary[parent(temp)]] = key;
+                            break;
+                        }
+                        else
+                        {
+                        }
+                    }
+                }
+                else if (temp % 2 == 1)//boş yer sağ çocukta
+                {
+                    adress[(int)binary[temp]] = adress[(int)binary[parent(temp)]];
+                    adress[(int)binary[parent(temp)]] = key;
+                    temp = parent(temp);
+                }
+            }
+            if (adress[(int)binary[binary.Count - 1]] == -1)
+                adress[(int)binary[binary.Count - 1]] = key;
+        }
+
+        public void oku()
+        { 
+            StreamReader dosyaOku;
+            dosyaOku=File.OpenText("kayit.txt");
+            for (int i = 0; i < 300; i++)
+                sayilar[i] = Convert.ToInt32(dosyaOku.ReadLine());
+            dosyaOku.Close();
+        } 
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //DosyayaYaz();
+            for (int i = 0; i < 307; i++)
+            {
+                adresbox.Text += i + "\n";
+            }
+            for (int i = 0; i < 307; i++)
+                adress[i] = -1;
+            for (int i = 0; i < 300; i++)
+                insert(sayilar[i]);
+
+            for (int i = 0; i < adress.Length; i++)
+                printbox.Text += adress[i] + "\n";
+        }
+    }
+}
